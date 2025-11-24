93011a71df17a610ee9252eb3c9e14fe8d85cdcb
Saurabh Jadhao <saurabh.jadhao@zimyo.com>
2025-11-24 10:21:38 +0530

    feat: Add new cloud regions and exchanges, and compact existing entries into a single-line format.

--- data/cloudRegions.ts
+++ data/cloudRegions.ts
@@ -1,4 +1,3 @@
-// data/cloudRegions.ts
 import type { CloudProvider } from "./exchanges";
 
 export type CloudRegion = {
@@ -11,65 +10,23 @@ export type CloudRegion = {
 };
 
 export const cloudRegions: CloudRegion[] = [
-    // AWS
-    {
-        id: "aws-mumbai",
-        provider: "AWS",
-        name: "AWS Mumbai",
-        regionCode: "ap-south-1",
-        lat: 19.076,
-        lng: 72.8777,
-    },
-    {
-        id: "aws-frankfurt",
-        provider: "AWS",
-        name: "AWS Frankfurt",
-        regionCode: "eu-central-1",
-        lat: 50.1109,
-        lng: 8.6821,
-    },
-    {
-        id: "aws-virginia",
-        provider: "AWS",
-        name: "AWS N. Virginia",
-        regionCode: "us-east-1",
-        lat: 39.0438,
-        lng: -77.4874,
-    },
+    // ---------- AWS ----------
+    { id: "aws-mumbai", provider: "AWS", name: "AWS Mumbai", regionCode: "ap-south-1", lat: 19.076, lng: 72.8777 },
+    { id: "aws-frankfurt", provider: "AWS", name: "AWS Frankfurt", regionCode: "eu-central-1", lat: 50.1109, lng: 8.6821 },
+    { id: "aws-virginia", provider: "AWS", name: "AWS N. Virginia", regionCode: "us-east-1", lat: 39.0438, lng: -77.4874 },
+    { id: "aws-ohio", provider: "AWS", name: "AWS Ohio", regionCode: "us-east-2", lat: 40.4173, lng: -82.9071 },
+    { id: "aws-singapore", provider: "AWS", name: "AWS Singapore", regionCode: "ap-southeast-1", lat: 1.3521, lng: 103.8198 },
+    { id: "aws-seattle", provider: "AWS", name: "AWS Oregon", regionCode: "us-west-2", lat: 45.5152, lng: -122.6784 },
 
-    // GCP
-    {
-        id: "gcp-singapore",
-        provider: "GCP",
-        name: "GCP Singapore",
-        regionCode: "asia-southeast1",
-        lat: 1.3521,
-        lng: 103.8198,
-    },
-    {
-        id: "gcp-frankfurt",
-        provider: "GCP",
-        name: "GCP Frankfurt",
-        regionCode: "europe-west3",
-        lat: 50.1109,
-        lng: 8.6821,
-    },
+    // ---------- GCP ----------
+    { id: "gcp-singapore", provider: "GCP", name: "GCP Singapore", regionCode: "asia-southeast1", lat: 1.3521, lng: 103.8198 },
+    { id: "gcp-frankfurt", provider: "GCP", name: "GCP Frankfurt", regionCode: "europe-west3", lat: 50.1109, lng: 8.6821 },
+    { id: "gcp-tokyo", provider: "GCP", name: "GCP Tokyo", regionCode: "asia-northeast1", lat: 35.6762, lng: 139.6503 },
+    { id: "gcp-iowa", provider: "GCP", name: "GCP Iowa", regionCode: "us-central-1", lat: 41.878, lng: -93.0977 },
 
-    // Azure
-    {
-        id: "azure-frankfurt",
-        provider: "AZURE",
-        name: "Azure Germany West Central",
-        regionCode: "de-west-central",
-        lat: 50.1109,
-        lng: 8.6821,
-    },
-    {
-        id: "azure-london",
-        provider: "AZURE",
-        name: "Azure UK South",
-        regionCode: "uk-south",
-        lat: 51.5072,
-        lng: -0.1276,
-    },
+    // ---------- Azure ----------
+    { id: "azure-frankfurt", provider: "AZURE", name: "Azure Germany West Central", regionCode: "de-west-central", lat: 50.1109, lng: 8.6821 },
+    { id: "azure-london", provider: "AZURE", name: "Azure UK South", regionCode: "uk-south", lat: 51.5072, lng: -0.1276 },
+    { id: "azure-dublin", provider: "AZURE", name: "Azure Ireland", regionCode: "eu-west", lat: 53.3498, lng: -6.2603 },
+    { id: "azure-south-korea", provider: "AZURE", name: "Azure Seoul", regionCode: "kr-south", lat: 37.5665, lng: 126.978 },
 ];
--- data/exchanges.ts
+++ data/exchanges.ts
@@ -1,4 +1,3 @@
-// data/exchanges.ts
 export type CloudProvider = "AWS" | "GCP" | "AZURE";
 
 export type Exchange = {
@@ -13,54 +12,33 @@ export type Exchange = {
 };
 
 export const exchanges: Exchange[] = [
-    {
-        id: "binance-tokyo",
-        name: "Binance",
-        city: "Tokyo",
-        country: "Japan",
-        lat: 35.6762,
-        lng: 139.6503,
-        provider: "AWS",
-        regionCode: "ap-northeast-1",
-    },
-    {
-        id: "okx-singapore",
-        name: "OKX",
-        city: "Singapore",
-        country: "Singapore",
-        lat: 1.3521,
-        lng: 103.8198,
-        provider: "GCP",
-        regionCode: "asia-southeast1",
-    },
-    {
-        id: "deribit-amsterdam",
-        name: "Deribit",
-        city: "Amsterdam",
-        country: "Netherlands",
-        lat: 52.3676,
-        lng: 4.9041,
-        provider: "AWS",
-        regionCode: "eu-west-1",
-    },
-    {
-        id: "bybit-frankfurt",
-        name: "Bybit",
-        city: "Frankfurt",
-        country: "Germany",
-        lat: 50.1109,
-        lng: 8.6821,
-        provider: "AZURE",
-        regionCode: "eu-central",
-    },
-    {
-        id: "binance-virginia",
-        name: "Binance",
-        city: "Ashburn",
-        country: "USA",
-        lat: 39.0438,
-        lng: -77.4874,
-        provider: "AWS",
-        regionCode: "us-east-1",
-    },
+    // Binance
+    { id: "binance-tokyo", name: "Binance", city: "Tokyo", country: "Japan", lat: 35.6762, lng: 139.6503, provider: "AWS", regionCode: "ap-northeast-1" },
+    { id: "binance-virginia", name: "Binance", city: "Ashburn", country: "USA", lat: 39.0438, lng: -77.4874, provider: "AWS", regionCode: "us-east-1" },
+
+    // OKX
+    { id: "okx-singapore", name: "OKX", city: "Singapore", country: "Singapore", lat: 1.3521, lng: 103.8198, provider: "GCP", regionCode: "asia-southeast1" },
+
+    // Deribit
+    { id: "deribit-amsterdam", name: "Deribit", city: "Amsterdam", country: "Netherlands", lat: 52.3676, lng: 4.9041, provider: "AWS", regionCode: "eu-west-1" },
+
+    // Bybit
+    { id: "bybit-frankfurt", name: "Bybit", city: "Frankfurt", country: "Germany", lat: 50.1109, lng: 8.6821, provider: "AZURE", regionCode: "eu-central" },
+
+    // Coinbase
+    { id: "coinbase-virginia", name: "Coinbase", city: "Ashburn", country: "USA", lat: 39.0438, lng: -77.4874, provider: "AWS", regionCode: "us-east-1" },
+    { id: "coinbase-london", name: "Coinbase", city: "London", country: "UK", lat: 51.5072, lng: -0.1276, provider: "AZURE", regionCode: "uk-south" },
+
+    // Kraken
+    { id: "kraken-dublin", name: "Kraken", city: "Dublin", country: "Ireland", lat: 53.3498, lng: -6.2603, provider: "AZURE", regionCode: "eu-west" },
+    { id: "kraken-seattle", name: "Kraken", city: "Seattle", country: "USA", lat: 47.6062, lng: -122.3321, provider: "AWS", regionCode: "us-west-2" },
+
+    // Bitget
+    { id: "bitget-singapore", name: "Bitget", city: "Singapore", country: "Singapore", lat: 1.3521, lng: 103.8198, provider: "GCP", regionCode: "asia-southeast1" },
+
+    // KuCoin
+    { id: "kucoin-hongkong", name: "KuCoin", city: "Hong Kong", country: "China", lat: 22.3193, lng: 114.1694, provider: "AWS", regionCode: "ap-east-1" },
+
+    // Huobi
+    { id: "huobi-seoul", name: "Huobi", city: "Seoul", country: "South Korea", lat: 37.5665, lng: 126.978, provider: "AZURE", regionCode: "kr-south" },
 ];
